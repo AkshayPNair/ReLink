3fc51bc4a56b5cd0ec9e3205720df2abd206c4db
Akshay <akshaypramod8486@gmail.com>
2025-11-02 12:41:41 +0530

    solved the cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://relink.akshaypnair.space", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
